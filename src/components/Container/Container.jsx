89bc17da68121e29bceb13bc7051bb19d2ad2b71
Dmitry Zaburunov <dzmitry3077@gmail.com>
2023-02-22 21:31:44 +0300

    Add Catalog, Footer, Order

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,6 +1,6 @@
 import s from './Container.module.css'
 import classNames from 'classnames'
 
-export const Container = ({ children }) => {
-  return <div className={classNames(s.container)}>{children}</div>
+export const Container = ({ children, className }) => {
+  return <div className={classNames(s.container, className)}>{children}</div>
 }
